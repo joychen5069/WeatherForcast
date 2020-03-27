21011ffd5155b87e462a7aea0245b2bf14fa242a
Joy Chen <joychen5069@gmail.com>
2020-03-27 19:00:07 -0400

    fixed queryURL

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
 console.log("linked to html")
+
+//API key
+const APIkey =  "db564efd201bdbb52479e58ddd22fc02"
+
+//URLS
+var city = "orlando"
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkey;
+//obviously change the city to whatever they searched
+
+console.log("city", city)
+console.log("queryURL", queryURL)
+
+
