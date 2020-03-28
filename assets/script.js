c1b1467f7795e9a011e0be76a28dab03bc950511
Joy Chen <joychen5069@gmail.com>
2020-03-28 18:42:45 -0400

    added temperature function in farenheit

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 console.log("linked to html")
 
 //API key
-const APIkey =  "db564efd201bdbb52479e58ddd22fc02"
+const APIkey = "db564efd201bdbb52479e58ddd22fc02"
 
 //URLS
 var city = $('#city-input').val();
@@ -11,16 +11,27 @@ var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&
 console.log("city", city)
 console.log("queryURL", queryURL)
 
-
-$('#find-city').on('click', function(event) {
+//search for a city
+$('#find-city').on('click', function (event) {
     //prevent submit button from submitting the form
     event.preventDefault();
 
     var city = $('#city-input').val();
+    $('#city-input').val('');
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey;
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkey;
-    
-    console.log("city", city)
-    console.log("queryURL", queryURL)
+    console.log("city", city);
+    console.log("queryURL", queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        $('.city-name').text(JSON.stringify(response.city.name))
+        console.log(response.list[0].main.temp)
+        $('.temp').text("Current Temperature: " + JSON.stringify(response.list[0].main.temp) + "°F")
+    })
 
 })
+
